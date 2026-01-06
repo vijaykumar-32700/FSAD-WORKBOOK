d0ef513e343fcbfbbbdcabb0a9e4f91417ab6bf5
2400032512@kluniversity.in <2400032700@kluniversity.in>
2026-01-06 21:07:41 +0530

    first commit

---
+++ experiment-1/main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+        System.out.println("Experiment 1 - Git Basics");
+        System.out.println("Feature Update Branch");
+        displayWelcomeMessage();
+    }
+    
+    public static void displayWelcomeMessage() {
+        System.out.println("Welcome to Git Branching!");
+    }
+}
